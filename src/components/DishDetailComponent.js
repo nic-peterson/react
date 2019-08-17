b4a415344e1b78949a4f3de53bc473ab33afebeb
Nic Peterson <nic.pete@gmail.com>
2019-08-17 15:14:36 -0700

    Assignment 1 Complete

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -15,19 +15,55 @@ class DishDetail extends Component {
 
   renderDish(dish) {
     return (
-      <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
+      <div className="container">
+        <div className="row">
+          <div className="col-5-md col-12-sm">
+            <Card>
+              <CardImg top src={dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+      </div>
     );
+  }
+
+  renderComment(dish) {
+    if (dish != null) {
+      const dishComments = dish.comments.map(commentDish => {
+        return (
+          <div>
+            <ul className="list-unstyled">
+              <li>{commentDish.comment}</li>
+              <li>
+                --{commentDish.author}, &nbsp;{" "}
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric"
+                }).format(new Date(commentDish.date))}
+              </li>
+            </ul>
+          </div>
+        );
+      });
+      return (
+        <div>
+          <h4>Comments</h4>
+          {dishComments}
+        </div>
+      );
+    } else {
+      return <div />;
+    }
   }
 
   render() {
     const dish = this.props.dish;
-    if (dish != null) return <div>{this.renderDish(dish)}</div>;
+    if (dish != null) return this.renderDish(dish);
     else return <div />;
   }
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -22,25 +22,40 @@ class Menu extends Component {
     this.setState({ selectedDish: dish });
   }
 
-  renderDish(dish) {
-    if (dish != null)
+  renderComment(dish) {
+    if (dish != null) {
+      const dishComments = dish.comments.map(commentDish => {
+        return (
+          <div>
+            <ul className="list-unstyled">
+              <li>{commentDish.comment}</li>
+              <li>
+                --{commentDish.author}, &nbsp;{" "}
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric"
+                }).format(new Date(commentDish.date))}
+              </li>
+            </ul>
+          </div>
+        );
+      });
       return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
+        <div>
+          <h4>Comments</h4>
+          {dishComments}
+        </div>
       );
-    else return <div />;
+    } else {
+      return <div />;
+    }
   }
 
   render() {
     const menu = this.props.dishes.map(dish => {
       return (
         <div className="col-12 col-md-5 m-1">
-          <DishDetail />
           <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
@@ -56,7 +71,10 @@ class Menu extends Component {
         <div className="row">{menu}</div>
         <div className="row">
           <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.state.selectedDish)}
+            <DishDetail dish={this.state.selectedDish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            {this.renderComment(this.state.selectedDish)}
           </div>
         </div>
       </div>
